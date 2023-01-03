35d1c7c16470be1054f505c2d72425927bd56981
Fabio Ricardo Zvir Junior <81338145+FabioZvir@users.noreply.github.com>
2023-01-03 17:32:32 -0300

    feed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 
 export const Footer = () => {
   return (
-    <div className={styles.footer}>Footer</div>
+    <div className={styles.footer}></div>
   )
 }
 
