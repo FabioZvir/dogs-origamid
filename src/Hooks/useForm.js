fd9734f192d1add1cd4f11e7dad0e9bae143420e
Fabio Ricardo Zvir Junior <81338145+FabioZvir@users.noreply.github.com>
2023-01-03 15:49:31 -0300

    photo

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -5,6 +5,10 @@ const types = {
     email: {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: 'Preencha um email válido'
+    },
+    number: {
+        regex: /^\d+$/,
+        message: 'Utilizi apenas numeros'
     }
 }
 
