eccde5e7752884cf3cfc2aa0b186a644c723e7aa
Fabio Ricardo Zvir Junior <81338145+FabioZvir@users.noreply.github.com>
2023-01-05 10:51:34 -0300

    CommentPostV1

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { UserContext } from '../../UserContext'
+import PhotoCommentsForm from './PhotoCommentsForm'
+import styles from './PhotoComments.module.css'
 
-const PhotoComments = () => {
+const PhotoComments = (props) => {
+  const [comments, setComments] = React.useState(() => props.comments)
+  const { login } = React.useContext(UserContext)
+
   return (
-    <div>PhotoComments</div>
+    <>
+      <ul className={styles.comment}>
+        {comments.map(comment =>
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}:</b>
+            <span>{comment.comment_content}</span>
+          </li>)}
+      </ul>
+      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+    </>
   )
 }
 
